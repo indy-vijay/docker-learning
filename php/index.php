7f053daf712c49b4af18928b4c8ad4862fa87c9f
indy <indy@2hatslogic.com>
2020-03-11 17:34:10 +0530

    fetch all results from index.php

--- php/index.php
+++ php/index.php
@@ -1,9 +1,9 @@
 <?php
 $user = "user";
 $pass = "1234";
-$dbh = new PDO('mysql:host=172.19.0.2;dbname=test_db', $user, $pass);
+$dbh = new PDO('mysql:host=db;dbname=test_db', $user, $pass);
 // use the connection here
 $sth = $dbh->query('SELECT * FROM users');
-print_r($sth->fetch());
+print_r($sth->fetchAll());
 
 var_dump($dbh);
